cf074d4da69299b292686f638ce8cde6e345ad30
Deeppatel213 <deeprpatel213@gmail.com>
2025-02-25 10:15:27 +0530

    add context into hooks

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,21 +1,29 @@
-import { setDefaultTimeout } from "@cucumber/cucumber";
+import { AfterAll, BeforeAll, setDefaultTimeout } from "@cucumber/cucumber";
 import { Before,After } from "@cucumber/cucumber";
-import { chromium,Browser,Page } from "@playwright/test";
+import { chromium,Browser,Page, BrowserContext } from "@playwright/test";
 import { pageFixure } from "./pageFixure";
 
 let page: Page;
 let browser: Browser;
+let context: BrowserContext;
+BeforeAll(async function() {
+    browser = await chromium.launch({headless:false})
+
+})
 
 Before(async function(){
-    browser = await chromium.launch({headless:false})
-    page = await browser.newPage()
+    context = await browser.newContext();
+    page = await browser.newPage();
     pageFixure.page = page;  
 });
 
 After(async function(){
-    await page.close()
-    await browser.close()
+    await page.close();
+    await context.close();
 })
 
+AfterAll(async function () {
+    await browser.close()
+})
 
 setDefaultTimeout(10*1000);

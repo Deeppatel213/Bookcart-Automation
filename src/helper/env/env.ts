39be493cf13a3ecac0cdbe59e6828752d4ca1562
Deeppatel213 <deeprpatel213@gmail.com>
2025-03-01 11:01:05 +0530

    Add Github Actions

--- src/helper/env/env.ts
+++ src/helper/env/env.ts
@@ -8,6 +8,6 @@ import * as dotenv from 'dotenv'
 export const getEnv = ()=>{
 dotenv.config({
     override: true,
-    path: `src/helper/env/.env.prod`
+    path: `src/helper/env/.env.${process.env.ENV}`
 })
 }
